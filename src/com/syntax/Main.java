720d579885ddda82c97faab4a99a5c8b2c0a0bad
mashsahakyan <wangtuan2804@gmail.com>
2022-02-09 13:28:24 -0500

    first commit

--- src/com/syntax/Main.java
+++ src/com/syntax/Main.java
@@ -2,7 +2,7 @@ package com.syntax;
 
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        for(int i = 0; i < 10; i++){
+        for(int i = 0; i < 5; i++){
             System.out.println("Batch 10 is great");
             Thread.sleep(2000);
         }
